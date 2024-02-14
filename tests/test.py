5f64a175ba0d615841c62ff9a25fa20924b16c3b
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-02-14 12:40:37 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,9 @@
+"""Module containing test cases for the package."""
 import unittest
 from datafetcher import Fetcher as ftc 
 
 class TestDataFetcher(unittest.TestCase):
-
+    """Module containing test cases for the package."""
     def test_historical_data_retrieval(self):
         """Test that historical data is retrieved without errors."""
         df = ftc("AAPL")
