d6453335c3091d112c2a2a046eff27e3b816476a
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-02-14 13:06:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     description='A comprehensive data retrieval aimed at gathering financial information',
-    long_description=open('README.md').read()   
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Giorgio Micaletto',
     author_email='giorgio.micaletto@studbocconi.it',
