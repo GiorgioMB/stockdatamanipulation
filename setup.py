e6c2e1af0f1d490f392526d3c8325d4513153b1f
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-02-15 10:17:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='stockdatafetcher',
+    name='stockdatamanipulation',
     version='0.1',
     packages=find_packages(),
     description='A comprehensive data retrieval aimed at gathering financial information',
