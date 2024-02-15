08ebbf48180e9322607a2db3de577204a0ee17e5
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-02-16 00:47:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stockdatamanager',
-    version='0.3',
+    version='0.3.1',
     packages=find_packages(),
     description='A comprehensive data retrieval aimed at gathering financial information',
     long_description=open('README.md').read(),
@@ -14,7 +14,9 @@ setup(
     install_requires=[
         'pandas>=1.1.5',
         'yfinance>=0.1.63',
-        'requests>=2.25.1'
+        'requests>=2.25.1',
+        'pandas-datareader>= 0.10.0'
+        'numpy>=1.23'
     ],
     python_requires='>=3.6',
 )
