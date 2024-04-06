4bed16102d8775af04eebc7a71b7955b2ab6a6cf
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-04-06 18:18:05 +0200

    Update __init__.py

--- stockdatamanager/__init__.py
+++ stockdatamanager/__init__.py
@@ -1,6 +1,3 @@
 from stockdatamanager.datafetcher import Fetcher
 from stockdatamanager.indicators import IndicatorCalculator
-from stockdatamanager.quant import Greeks
-from stockdatamanager.quant import OptionPricing
-
 __version__ = '0.8'
