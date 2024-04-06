88a4f722dfa8273efaa043599d4d294e6f0ac57e
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-04-06 21:50:11 +0200

    Update customerrors.py

--- stockdatamanager/customerrors.py
+++ stockdatamanager/customerrors.py
@@ -1,4 +1,7 @@
 class MethodError(Exception):
+    """
+    Internal class to raise an error when an invalid method is passed, for internal use only (or not, but I don't see the point)
+    """
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
