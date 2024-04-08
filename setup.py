9ac3b454569867c9e423d6193f0c4dbadb9c799c
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-04-08 11:42:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stockdatamanager',
-    version='0.9.1',
+    version='0.10',
     packages=find_packages(),
     description='A comprehensive library for financial analysis',
     long_description=open('README.md').read(),
