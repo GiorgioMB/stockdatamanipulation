1a149d8eaf6bf06b9ca898fa04df2d200a5abdaa
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-06-02 15:13:21 +0200

    Update __init__.py

--- stockdatamanager/__init__.py
+++ stockdatamanager/__init__.py
@@ -1,4 +1,4 @@
 from stockdatamanager.datafetcher import Fetcher
 from stockdatamanager.indicators import IndicatorCalculator
 from stockdatamanager.options import *
-__version__ = '1.1'
+__version__ = '1.1.1'
